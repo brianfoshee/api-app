76a0e8d6e06728440bdd5d491a141d0b3a6ee8af
Brian Foshee <brianfoshee@gmail.com>
2015-07-19 14:05:15 -0400

    use stdout for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Use STDOUT logger for Heroku
+  config.logger = Logger.new(STDOUT)
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
