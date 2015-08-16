4f5fe748364e892f1554f66204abb18c405b560e
Brian Foshee <brianfoshee@gmail.com>
2015-08-16 17:00:35 -0400

    move .env to pre dependency. simplify circle config.

--- dependencies.sh
+++ dependencies.sh
@@ -6,3 +6,6 @@ set -x
 echo "Installing docker-compose..."
 curl -L https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > /home/ubuntu/bin/docker-compose
 chmod +x /home/ubuntu/bin/docker-compose
+
+echo "Starting up containers..."
+docker-compose up -d
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-docker-compose up -d
-docker-compose run web /app/user/bin/rake db:create db:schema:load --trace
-docker-compose run web /app/user/bin/rake test
